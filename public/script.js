aa86593ccce3f305c0110d4a0b8e8521a1f3923e
Ac2zoom <ac2zoom@yahoo.com>
2016-10-16 12:31:33 -0700

    Akshay's changes'

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
-	var marker;
-	var gene;
-	var promoter;
+	var markers = [];
+	var genes = [];
+	var promoters = [];
 
 	$('.geneTool').click(function () {
 		$(this).css("opacity", 0.7);
@@ -9,78 +9,54 @@ $(document).ready(function () {
 
 
 	$('.geneTool p').click(function () {
-		marker = $(this).html(); //shouldn't this be gene = ? mixed up
+		var marker = $(this).html(); //shouldn't this be gene = ? mixed up
+		if (markers.indexOf(marker) > -1) {
+			markers.splice(markers.indexOf(marker), 1);
+		} else {
+			markers.push(marker);
+		}
 		console.log(marker);
 	});
 
 	$('.promoTool p').click(function () {
-		promoter = $(this).html();
+		var promoter = $(this).html();
+		if (promoters.indexOf(promoter) > -1) {
+			promoters.splice(promoters.indexOf(promoter), 1);
+		} else {
+			promoters.push(promoter);
+		}
 		console.log(promoter);
 	});
 
 	$('.markerTool p').click(function () {
-		gene = $(this).html(); //should be marker?
+		var gene = $(this).html(); //should be marker?
+		if (genes.indexOf(gene) > -1) {
+			genes.splice(genes.indexOf(gene), 1);
+		} else {
+			genes.push(gene);
+		}
 		console.log(gene);
 
 	});
 
 	$('button').click(function () {
-		if (level.level < 3) {
+		if (level.level < 5) {
 			for (var i = level.cases.length - 1; i >= 0; i--) {
-				if(gene == level.cases[i].input) { // && marker == cases[i].inputMarker
+				if(genes[0] == level.cases[i].input) { // && marker == cases[i].inputMarker
 					if(level.cases[i].isCorrect) {
 						console.log("Correct!");
 						break;
 					}
 				}
 			}
+		} else if (level.level < 6) {
+			// Lead Inducible Gene Puzzle (ensure that this logic is encoded in the db)
+
+		} else {
+
 		}
 	});
 
-	var casesOne = [
-
-            {
-                "inputGene": "red",
-                "inputMarker" : null,
-                "inputPromoter" : null,
-                "output": "red/wild",
-                "isCorrect": false
-            },
-            {
-                "inputGene": "yellow",
-                "inputMarker" : null,
-                "inputPromoter" : null,
-                "output": "yellow/wild",
-                "isCorrect": false
-            },
-            {
-                "inputGene": "blue",
-                "inputMarker" : null,
-                "inputPromoter" : null,
-                "output": "blue/wild",
-                "isCorrect": false
-            },
-            {
-                "inputGene": "blue",
-                "inputMarker" : ["KanMX", "NatMX", "BleoMX"],
-                "inputPromoter" : null,
-                "output": "blue",
-                "isCorrect": false
-            },
-            {
-                "inputGene": "red",
-                "inputMarker" : ["KanMX", "NatMX", "BleoMX"],
-                "inputPromoter" : null,
-                "output": "red",
-                "isCorrect": true
-            },
-            {
-                "inputGene": "yellow",
-                "inputMarker" : ["KanMX", "NatMX", "BleoMX"],
-                "inputPromoter" : null,
-                "output": "yellow",
-                "isCorrect": false
-            }];
     var casesTwo = [
             {
                 "inputGene": "yellow",

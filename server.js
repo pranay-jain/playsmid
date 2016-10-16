e082d188076eb985a992fbe7faac870e0797566a
Pranay Jain <pranayjain@D-10-18-234-9.dhcp4.washington.edu>
2016-10-16 05:40:06 -0700

    backend worksgit add .

--- server.js
+++ server.js
@@ -28,9 +28,9 @@ app.get('/play', (req, res) => {
 	var cursor = db.collection('data').find({'level': 0});
 	var level;
 	cursor.toArray(function(err, items) {
-		console.log(items);
+		console.log(items[0]);
 		//level = items;
-		res.render('play', {'level': items, 'string': "hello world"});
+		res.render('play', {'level': items[0], 'string': "hello world"});
 	});
 	//console.log(level);
 	
